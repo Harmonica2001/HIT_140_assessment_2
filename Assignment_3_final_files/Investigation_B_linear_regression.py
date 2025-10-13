197949a69261a410d412d216e077bdb53f7c8086
Syed Haroon Ahmad <167967943+Harmonica2001@users.noreply.github.com>
2025-10-13 18:20:18 +0930

    Investigation B jupyter file

--- Assignment_3_final_files/Investigation_B_linear_regression.py
+++ Assignment_3_final_files/Investigation_B_linear_regression.py
@@ -12,8 +12,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import statsmodels.api as sm
 import seaborn as sns
-df = pd.read_csv("Datasets/dataset2_cleaned_V3.csv")
+df = pd.read_csv("C:\Personal\Masters\Masters_work\Study\Y1_S1\HIT140\Assessment_2\HIT_140_Assessment_2_200925\HIT_140_assessment_2\Datasets/dataset2_cleaned_V3.csv")
 df = df.drop(columns=['time'])
+#%%
 #%%
 df.head()
 # df.info()
@@ -40,7 +41,16 @@ Extra:
     '''
 
 #%%
+# scatter plot
 
+df5 = pd.read_csv("Datasets/dataset1.csv")
+plt.scatter(df5["month"], df5["season"], c="C0", alpha=0.8, edgecolor="k")
+plt.xlabel("Month")
+plt.ylabel("Season")
+plt.title("Month vs season")
+plt.grid(True)
+plt.show()
+#%%
 corr = df.corr()
 plt.figure(figsize=(8, 6))
 sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm", vmin=-1, vmax=1,
@@ -156,21 +166,25 @@ print("R^2: ", r_2)
 
 ### splitting the dataset into separate seasons ###
 #%%
-df_spr = df[df['season_code'] == 1]
-df_win = df[df['season_code'] == 0]
+df_spr = df2[df2['season_code'] == 1]
+df_win = df2[df2['season_code'] == 0]
+df_spr = df_spr.drop(columns=['season_code'])
+df_win = df_win.drop(columns=['season_code'])
 df_spr = df_spr.dropna()
 df_win = df_win.dropna()
 #%% Spring( months = 3,4,5) ###
 x_spr = df_spr.drop(columns=['bat_landing_number'])
 y_spr = df_spr['bat_landing_number']             # e.g., y_col = 'y'
 # # fit
-model = sm.OLS(y, x_spr).fit()
+model = sm.OLS(y_spr, x_spr).fit()
 # # summary
 print(model.summary())
 #%% Winter( months = 3,4,5) ###
 x_win = df_win.drop(columns=['bat_landing_number'])
 y_win = df_win['bat_landing_number']             # e.g., y_col = 'y'
 # # fit
-model = sm.OLS(y, x_spr).fit()
+model = sm.OLS(y_win, x_win).fit()
 # # summary
 print(model.summary())
+
+# %%
